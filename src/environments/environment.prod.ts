49a11706efb438b36b829f5f5281110eb8f70417
mrkratin <91769235+mrkratin@users.noreply.github.com>
2021-10-02 01:28:00 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://159.203.99.219/',
-  urlApi: 'http://159.203.99.219/api/',
-  urlAuth: 'http://159.203.99.219/api/auth/',
+  url: 'https://apis-bruce.online/',
+  urlApi: 'https://apis-bruce.online/api/',
+  urlAuth: 'https://apis-bruce.online/api/auth/',
 };
